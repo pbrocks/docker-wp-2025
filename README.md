4a54c892f284718f83f52870f918f269166625ef
Paul Barthmaier <pbrocks@users.noreply.github.com>
2025-01-06 21:25:40 -0500

    initial docker containers

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # docker-wp-2025
+
 Docker containers to spin up WordPress installation and provide database access as well as local mail server.
+
+d
---
+++ developing/developing.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Developing WordPress
+ * Plugin URL: https://github.com/pbrocks/tag-docker-wp
+ * Author Name: pbrocks
+ * Author URL: https://github.com/pbrocks/tag-docker-wp
+ */
