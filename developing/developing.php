8ff9b51dd076243262f2b0335672aaea632d92fe
Paul Barthmaier <pbrocks@users.noreply.github.com>
2025-01-07 07:40:06 -0500

    Update developing.php

--- developing/developing.php
+++ developing/developing.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Developing WordPress
  * Plugin URL: https://github.com/pbrocks/tag-docker-wp
- * Description: Folder in repository mapped to <code>/plugins</code> folder in Docker container creates WordPress plugin.
+ * Description: A folder in repository mapped to <code>/plugins</code> folder in Docker container creates WordPress plugin.
  * Author Name: pbrocks
  * Author URL: https://github.com/pbrocks/tag-docker-wp
  */
